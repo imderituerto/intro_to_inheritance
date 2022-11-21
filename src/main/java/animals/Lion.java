f33a04397e151731dc2108846e59085509092908
Isabel Martinez de Rituerto <imderituerto@Isabels-MacBook-Pro.local>
2022-11-21 11:28:46 +0100

    Created Lion test

--- src/main/java/animals/Lion.java
+++ src/main/java/animals/Lion.java
@@ -10,4 +10,5 @@ public class Lion extends Animal{
         super(name);
     }
 
+
 }
--- src/test/java/animals/LionTest.java
+++ src/test/java/animals/LionTest.java
@@ -1,5 +1,24 @@
 package animals;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LionTest {
 
+    private Lion lion;
+
+    @BeforeEach
+    public void setUp(){
+        lion = new Lion("Scar");
+    }
+
+    @Test
+    public void makeNoise(){
+        String expected = "Hello, my name is Scar."; //Arrange -- input the expected outcome.
+        String actual = lion.makeNoise(); // Act -- call the method you want to test.
+        assertEquals(expected, actual);
+        // asserThat(actual).isEqualTo(expected)
+    }
 }
